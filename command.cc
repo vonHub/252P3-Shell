4eddc26990d26df91ab5bfbbbaa304c73c82cdb0
CS252 <cs252@cs.purdue.edu>
2016-02-15 22:15:51 -0500

    Total:                   3   of 100

---
+++ command.cc
@@ -0,0 +1,175 @@
+
+/*
+ * CS252: Shell project
+ *
+ * Template file.
+ * You will need to add more code here to execute the command table.
+ *
+ * NOTE: You are responsible for fixing any bugs this code may have!
+ *
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <string.h>
+#include <signal.h>
+
+#include "command.h"
+
+SimpleCommand::SimpleCommand()
+{
+	// Create available space for 5 arguments
+	_numOfAvailableArguments = 5;
+	_numOfArguments = 0;
+	_arguments = (char **) malloc( _numOfAvailableArguments * sizeof( char * ) );
+}
+
+void
+SimpleCommand::insertArgument( char * argument )
+{
+	if ( _numOfAvailableArguments == _numOfArguments  + 1 ) {
+		// Double the available space
+		_numOfAvailableArguments *= 2;
+		_arguments = (char **) realloc( _arguments,
+				  _numOfAvailableArguments * sizeof( char * ) );
+	}
+	
+	_arguments[ _numOfArguments ] = argument;
+
+	// Add NULL argument at the end
+	_arguments[ _numOfArguments + 1] = NULL;
+	
+	_numOfArguments++;
+}
+
+Command::Command()
+{
+	// Create available space for one simple command
+	_numOfAvailableSimpleCommands = 1;
+	_simpleCommands = (SimpleCommand **)
+		malloc( _numOfSimpleCommands * sizeof( SimpleCommand * ) );
+
+	_numOfSimpleCommands = 0;
+	_outFile = 0;
+	_inFile = 0;
+	_errFile = 0;
+	_background = 0;
+}
+
+void
+Command::insertSimpleCommand( SimpleCommand * simpleCommand )
+{
+	if ( _numOfAvailableSimpleCommands == _numOfSimpleCommands ) {
+		_numOfAvailableSimpleCommands *= 2;
+		_simpleCommands = (SimpleCommand **) realloc( _simpleCommands,
+			 _numOfAvailableSimpleCommands * sizeof( SimpleCommand * ) );
+	}
+	
+	_simpleCommands[ _numOfSimpleCommands ] = simpleCommand;
+	_numOfSimpleCommands++;
+}
+
+void
+Command:: clear()
+{
+	for ( int i = 0; i < _numOfSimpleCommands; i++ ) {
+		for ( int j = 0; j < _simpleCommands[ i ]->_numOfArguments; j ++ ) {
+			free ( _simpleCommands[ i ]->_arguments[ j ] );
+		}
+		
+		free ( _simpleCommands[ i ]->_arguments );
+		free ( _simpleCommands[ i ] );
+	}
+
+	if ( _outFile ) {
+		free( _outFile );
+	}
+
+	if ( _inFile ) {
+		free( _inFile );
+	}
+
+	if ( _errFile ) {
+		free( _errFile );
+	}
+
+	_numOfSimpleCommands = 0;
+	_outFile = 0;
+	_inFile = 0;
+	_errFile = 0;
+	_background = 0;
+}
+
+void
+Command::print()
+{
+	printf("\n\n");
+	printf("              COMMAND TABLE                \n");
+	printf("\n");
+	printf("  #   Simple Commands\n");
+	printf("  --- ----------------------------------------------------------\n");
+	
+	for ( int i = 0; i < _numOfSimpleCommands; i++ ) {
+		printf("  %-3d ", i );
+		for ( int j = 0; j < _simpleCommands[i]->_numOfArguments; j++ ) {
+			printf("\"%s\" \t", _simpleCommands[i]->_arguments[ j ] );
+		}
+	}
+
+	printf( "\n\n" );
+	printf( "  Output       Input        Error        Background\n" );
+	printf( "  ------------ ------------ ------------ ------------\n" );
+	printf( "  %-12s %-12s %-12s %-12s\n", _outFile?_outFile:"default",
+		_inFile?_inFile:"default", _errFile?_errFile:"default",
+		_background?"YES":"NO");
+	printf( "\n\n" );
+	
+}
+
+void
+Command::execute()
+{
+	// Don't do anything if there are no simple commands
+	if ( _numOfSimpleCommands == 0 ) {
+		prompt();
+		return;
+	}
+
+	// Print contents of Command data structure
+	print();
+
+	// Add execution here
+	// For every simple command fork a new process
+	// Setup i/o redirection
+	// and call exec
+
+	// Clear to prepare for next command
+	clear();
+	
+	// Print new prompt
+	prompt();
+}
+
+// Shell implementation
+
+void
+Command::prompt()
+{
+	printf("myshell>");
+	fflush(stdout);
+}
+
+Command Command::_currentCommand;
+SimpleCommand * Command::_currentSimpleCommand;
+
+int yyparse(void);
+
+main()
+{
+	Command::_currentCommand.prompt();
+	yyparse();
+}
+
