874d1c427dca53dec3fed4252224208f3c6c1799
Chris Von Hoene <cvonhoen@purdue.edu>
2016-03-23 14:29:34 -0400

    Total:                   43  of 100

--- command.cc
+++ command.cc
@@ -299,7 +299,7 @@ Command::execute()
 
             if (!strcmp(_simpleCommands[i]->_arguments[0], "cd")) {
                 // Change directory and exit
-                chdir(simpleCommands[i]->_arguments[1]);
+                chdir(_simpleCommands[i]->_arguments[1]);
                 exit(0);
             }
             execvp(_simpleCommands[i]->_arguments[0], _simpleCommands[i]->_arguments);
